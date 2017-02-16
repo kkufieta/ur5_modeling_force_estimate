0b5b6795deda9a8c2bb043a0a2e82bbfebb2543c
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2017-02-15 22:19:39 -0500

    fix: delete unnecessary files

--- JointSpaceControl_Model_B/Angels_Implementation/J_a.m
+++
@@ -1,7 +0,0 @@
-function J_a = J_a(q,robot)
-% Calculate analytical Jacobian
-
-J_a = robot.jacob0(q,'eul');
-
-end
-
--- JointSpaceControl_Model_C_ForceEstim/Angels_Implementation/dJ_a_dq.m
+++
@@ -1,7 +0,0 @@
-function dJ_a_dq = dJ_a_dq(q,dq,robot)
-% Calculate analytical Jacobian
-
-dJ_a_dq = robot.jacob_dot(q,dq);
-
-end
-
